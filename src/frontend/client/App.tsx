dc518ec0617afcf5b0c546e344007c31a43d589b
paranoidFrappe <delectable999@gmail.com>
2023-07-21 20:37:01 -0700

    Initial commit

---
+++ src/frontend/client/App.tsx
@@ -0,0 +1,22 @@
+import React ,{FC}from "react";
+// import { render } from 'react-dom';
+
+const App: FC = () => {
+  return (
+    <div>
+      <h1>Testing</h1>
+    </div>
+  )
+}
+
+export default App
+
+
+{/* export default function App() {
+  return (
+    <>
+      <h1>Hello World!!!</h1>
+    </>
+  );
+} */}
+
