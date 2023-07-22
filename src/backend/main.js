67df09164cbb61adcaddbd8bd81b334448698151
paranoidFrappe <delectable999@gmail.com>
2023-07-22 16:14:59 -0700

    v1.0.9

--- src/backend/main.js
+++ src/backend/main.js
@@ -11,23 +11,25 @@ app.get('/', (req, res) => {
 });
 
 
-    
-
 function createWindow() {  
   // Create a new window
   let window = new electron_1.BrowserWindow({
-      width: 750,
-      height: 700,
-      show: false,
-      title: 'Bench Test Pro',
-      icon: path.join(__dirname, './icon.png'),
+    width: 750,
+    height: 700,
+    show: false,
+    title: 'Bench Test Pro',
+    icon: path.join(__dirname, './icon.png'),
   });
 
+  // This platform is checking to see if the OS is Mac, and setting the icon
+  if (process.platform === 'darwin') {
+    app.dock.setIcon(path.join(__dirname, './icon.png'));
+  }
 
   // Event listeners on the window
   window.webContents.on("did-finish-load", () => {
-      window.show();
-      window.focus();
+    window.show();
+    window.focus();
   });
 
   indexPath = url.format({
@@ -45,13 +47,12 @@ electron_1.app.whenReady().then(() => {
   createWindow();
 
   electron_1.app.on("activate", () => {
-      // On macOS it's common to re-create a window in the app when the
-      // dock icon is clicked and there are no other windows open.
-      if (electron_1.BrowserWindow.getAllWindows().length === 0) {
-          createWindow();
-      }
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (electron_1.BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
   });
-
 });
 
 
@@ -60,6 +61,6 @@ electron_1.app.whenReady().then(() => {
 // explicitly with Cmd + Q.
 electron_1.app.on("window-all-closed", function () {
   if (process.platform !== "darwin") {
-      app.quit();
+    app.quit();
   }
 });
